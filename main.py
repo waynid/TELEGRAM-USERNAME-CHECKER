5433136b65c3a2d9ef9ab487b2fc80a16dadec5a
WAYNID <159060343+waynid@users.noreply.github.com>
2025-04-30 17:12:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from colorama import Fore, init
 
 init(autoreset=True)
 
+def fragmentcheck(username):
+    url = f'https://fragment.com/username/{username}'
+    response = requests.get(url, allow_redirects=True)
+
+    if response.url.startswith("https://fragment.com/?query="):
+        return False  
+    if '<span class="tm-section-header-status tm-status-avail">Available</span>' in response.text:
+        return True  
+    if '<span class="tm-section-header-status tm-status-taken">Taken</span>' in response.text:
+        return True  
+    return None  
+
 with open('input.txt', 'r', encoding='utf-8') as f:
     usernames = [line.strip() for line in f if line.strip()]
 
@@ -10,7 +22,23 @@ with open('output.txt', 'w', encoding='utf-8') as output:
     for username in usernames:
         r = requests.get(f'https://t.me/{username}')
         if '<i class="tgme_icon_user"></i>' in r.text:
-            print(Fore.GREEN + f"[AVAILABLE] {username}")
-            output.write(username + '\n')
+            
+            fragment = fragmentcheck(username)
+
+            if fragment is False:
+                status = "AVAILABLE"
+                color = Fore.GREEN
+                print(color + f"[{status}] {username}")
+                output.write(f"{username}\n")  
+            elif fragment is None:
+                status = "UNKNOWN"
+                color = Fore.YELLOW
+                print(color + f"[{status}] {username}")
+            else:
+                status = "TAKEN"
+                color = Fore.RED
+                print(color + f"[{status}] {username}")
         else:
-            print(Fore.RED + f"[TAKEN] {username}")
+            status = "TAKEN"
+            color = Fore.RED
+            print(color + f"[{status}] {username}")
